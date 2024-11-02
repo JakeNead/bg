afa0a01f9e02ed1da33b6d68a062eb8a4f586230
Jake Nead <jakenead@gmail.com>
2024-11-01 19:02:21 -0700

    Use CssBaseline

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import * as React from "react";
+import Button from "@mui/material/Button";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <p>test</p>;
+  return <Button variant="contained">Hello Spruce</Button>;
 }
 
 export default App;
